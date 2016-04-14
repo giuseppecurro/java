1effa71c899bb6263be81da295e779094c621fee
giuseppecurro <giuseppe.curro1@gmail.com>
2016-04-14 17:17:27 +0200

    sostituito  metodo show...

--- 4b_0/esempio1.java
+++ 4b_0/esempio1.java
@@ -1,29 +1,24 @@
+//primi passi con javax.swing gio 14 apr 16
 import java.awt.*;
 import javax.swing.*;
 public class esempio1{
         public static void main(String[] v){
-            JFrame f = new JFrame("Esempio 1");
+            JFrame f        = new JFrame("Esempio 1");
             f.setBounds(200,100,600,300);
             Container c = f.getContentPane();
-            JPanel p = new JPanel();
-            JLabel lb1 = new JLabel("Etichetta");
-           JButton b = new JButton("conferma");
-           JRadioButton jr = new JRadioButton("false");
-           JTextField txt = new JTextField("input dati");
+            JPanel p =      new JPanel();
+            JLabel lb1 =    new JLabel("Etichetta");
+            JButton b =     new JButton("conferma");
+            JRadioButton jr = new JRadioButton("false");
+            JTextField txt  = new JTextField("input dati");   
 
+               c.add(p);
+               p.add(lb1);
+               p.add(b);
+               p.add(jr);
+               p.add(txt);
 
-           c.add(p);
-           p.add(lb1);
-           p.add(b);
-           p.add(jr);
-           p.add(txt);
-
-
-
-
-
-
-            f.show();
+            f.setSize(639,199);
+            f.setVisible(true);//al posto di f.show()
         }
-
 }
