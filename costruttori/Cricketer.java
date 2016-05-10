cace7a4028e89c8a816189c65bd15f6fe198caf4
giuseppecurro <giuseppe.curro1@gmail.com>
2016-05-10 10:27:04 +0200

    costruttori

---
+++ costruttori/Cricketer.java
@@ -0,0 +1,32 @@
+class Cricketer { 
+         String name; 
+         String team; 
+         int age; 
+         
+         Cricketer ()    //default constructor . 
+         { 
+          name =""; 
+          team =""; 
+          age = 0; 
+         } 
+         
+         Cricketer(String n, String t, int a)    //constructor overloaded  
+         { 
+          name = n; 
+          team = t; 
+          age = a; 
+         } 
+         
+         Cricketer (Cricketer ckt)      //constructor similar to copy constructor of c++   
+         { 
+          name = ckt.name; 
+          team = ckt.team; 
+          age = ckt.age; 
+         } 
+         
+         public String toString()  
+         { 
+          return "this is " + name + " of "+team; 
+         } 
+} 
+ 
