d8c55acece4d98cb6a9378b3f6b33a92a41fab94
giuseppecurro <giuseppe.curro1@gmail.com>
2016-05-17 12:30:06 +0200

    ipotenusa

---
+++ ipotenusa/Ipotenusa.java
@@ -0,0 +1,10 @@
+public class Ipotenusa {
+    private double c1,c2;
+  
+    
+    double calcola(double c1,double c2){
+        return (Math.sqrt(c1*c1+c2*c2));
+    
+    }
+
+}
---
+++ ipotenusa/Usa_ipo.java
@@ -0,0 +1,10 @@
+public class Usa_ipo {
+
+    public static void main(String[] args){
+        Ipotenusa i = new Ipotenusa();
+        System.out.println(i.calcola(3,4));
+        
+        
+    }
+
+}
